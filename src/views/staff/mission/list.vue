6ea594cc083de5d475e06d45811d5328d453275b
zhuwj <767839489@qq.com>
2017-12-11 22:48:27 +0800

    修改加载列表方式

--- src/views/staff/mission/list.vue
+++ src/views/staff/mission/list.vue
@@ -7,37 +7,33 @@
   </tab>
 
   <div v-if="lists.length > 0" style="-webkit-overflow-scrolling: touch">
-    <mt-loadmore :bottom-method="loadBottom" :bottom-all-loaded="allLoaded" :auto-fill="false" ref="loadmore" @bottom-status-change="handleBottomChange">
-        <group v-for="(item, index) in lists" :key="index" gutter="8px">
-            <cell :title="item.realname" :inline-desc="'下单时间: ' + item.created_at" style="border-bottom: 1px solid #D9D9D9;">
-                <img slot="icon" :src="item.avatar" class="avatar" />
-                <a :href="'tel:'+item.mobile">{{item.mobile}}</a>
-            </cell>
+    <group v-for="(item, index) in lists" :key="index" gutter="8px">
+        <cell :title="item.realname" :inline-desc="'下单时间: ' + item.created_at" style="border-bottom: 1px solid #D9D9D9;">
+            <img slot="icon" :src="item.avatar" class="avatar" />
+            <a :href="'tel:'+item.mobile">{{item.mobile}}</a>
+        </cell>
 
-            <cell :title="item.express_com+' '+item.pickup_code+' '+(item.upstairs_price > 0 ? '送到宿舍' : '')" @click.native="toDetail(item.id)">
-                <x-icon slot="icon" type="android-plane" class="g-icon" size="20" style="fill: rgb(65, 194, 215)"></x-icon>
-            </cell>
-            <cell :title="item.college+' '+item.area+' '+item.detail" @click.native="toDetail(item.id)">
-                <x-icon slot="icon" type="location" class="g-icon" size="20"></x-icon>
-            </cell>
-            <cell :title="'送达时间: '+item.arrive_time" @click.native="toDetail(item.id)">
-                <x-icon slot="icon" type="ios-clock-outline" class="g-icon" size="20" style="fill: rgb(255, 49, 0)"></x-icon>
-            </cell>
+        <cell :title="item.express_com+' '+item.pickup_code+' '+(item.upstairs_price > 0 ? '送到宿舍' : '')" @click.native="toDetail(item.id)">
+            <x-icon slot="icon" type="android-plane" class="g-icon" size="20" style="fill: rgb(65, 194, 215)"></x-icon>
+        </cell>
+        <cell :title="item.college+' '+item.area+' '+item.detail" @click.native="toDetail(item.id)">
+            <x-icon slot="icon" type="location" class="g-icon" size="20"></x-icon>
+        </cell>
+        <cell :title="'送达时间: '+item.arrive_time" @click.native="toDetail(item.id)">
+            <x-icon slot="icon" type="ios-clock-outline" class="g-icon" size="20" style="fill: rgb(255, 49, 0)"></x-icon>
+        </cell>
 
-            <cell>
-                <x-icon slot="icon" type="cash" class="g-icon" size="20"></x-icon>
-                <span slot="title" class="text-danger">￥{{item.total_price}}</span>
-                <span v-if="isStaff">
-                    <x-button mini type="warn" @click.native="acceptOrder(item.id)" v-if="item.status === '待接单'">立即接单</x-button>
-                </span>
-            </cell>
-        </group>
+        <cell>
+            <x-icon slot="icon" type="cash" class="g-icon" size="20"></x-icon>
+            <span slot="title" class="text-danger">￥{{item.total_price}}</span>
+            <span v-if="isStaff">
+                <x-button mini type="warn" @click.native="acceptOrder(item.id)" v-if="item.status === '待接单'">立即接单</x-button>
+            </span>
+        </cell>
+    </group>
 
-        <div slot="bottom" class="loadmore-bottom">
-              <span v-show="bottomStatus === 'loading'">加载中<inline-loading></inline-loading></span>
-              <span v-show="allLoaded === true">没有更多了</span>
-        </div>
-    </mt-loadmore>
+    <x-button class="loadmore-bottom" @click.native="loadData()" v-show="!loading && !allLoaded">点击加载更多</x-button>
+    <load-more :show-loading="loading" :tip="loadMoreTip" background-color="#fbf9fe" v-show="loading || allLoaded"></load-more>
   </div>
 
   <no-content title="暂无订单" v-else>
@@ -47,8 +43,7 @@
 </template>
 
 <script>
-import { Loadmore as MtLoadmore } from 'mint-ui'
-import { Group, Cell, XButton, Tab, TabItem, InlineLoading } from 'vux'
+import { Group, Cell, XButton, Tab, TabItem, InlineLoading, LoadMore } from 'vux'
 import mixin from 'src/mixins/expressMission.js'
 import NoContent from 'src/components/NoContent'
 
@@ -68,18 +63,18 @@ export default {
       currentPage: 0,
       lists: [],
       allLoaded: false,
-      bottomStatus: ''
+      loading: false
     }
   },
   components: {
-    MtLoadmore,
     Group,
     Cell,
     XButton,
     Tab,
     TabItem,
     NoContent,
-    InlineLoading
+    InlineLoading,
+    LoadMore
   },
   created () {
     this.loadData(true)
@@ -95,6 +90,13 @@ export default {
         status: this.currentStatus,
         page: this.currentPage
       }
+    },
+    loadMoreTip () {
+      if (this.loading && !this.allLoaded) {
+        return '正在加载中'
+      } else {
+        return '没有更多了'
+      }
     }
   },
   mixins: [mixin],
@@ -104,52 +106,45 @@ export default {
     next()
   },
   methods: {
-    loadBottom () {
-      this.loadData()
-      this.$refs.loadmore.onBottomLoaded()
-    },
     async loadData (loading = false) {
-      this.$store.commit('UPDATE_LOADING_STATUS', { title: '加载中...', status: loading })
+      this.currentPage += 1
+      this.loading = true
 
-      setTimeout(() => {
-        this.currentPage += 1
+      if (!this.currentStatus) {
+        this.currentStatus = this.$route.params.status
+      }
 
-        if (!this.currentStatus) {
-          this.currentStatus = this.$route.params.status
+      await this.$http.get('/missions', { params: this.queryParams }).then(res => {
+        const member = res.meta.member
+
+        this.totalPages = res.meta.pagination.total_pages
+
+        if (member.is_staff) {
+          this.lists = [...this.lists, ...res.data]
+          this.isStaff = true
+        } else {
+          this.lists = res.data.map(item => ({
+            id: item.id,
+            realname: item.realname.slice(0, 1) + '**',
+            mobile: '',
+            created_at: item.created_at,
+            avatar: item.avatar,
+            express_com: item.express_com,
+            express_type: item.express_type,
+            college: item.college,
+            area: item.area,
+            detail: item.detail,
+            arrive_time: item.arrive_time,
+            total_price: item.total_price
+          }))
         }
+      })
 
-        this.$http.get('/missions', { params: this.queryParams }).then(res => {
-          const member = res.meta.member
+      if (this.currentPage === this.totalPages) {
+        this.allLoaded = true
+      }
 
-          this.totalPages = res.meta.pagination.total_pages
-
-          if (member.is_staff) {
-            this.lists = this.lists.concat(res.data)
-            this.isStaff = true
-          } else {
-            this.lists = res.data.map(item => ({
-              id: item.id,
-              realname: item.realname.slice(0, 1) + '**',
-              mobile: '',
-              created_at: item.created_at,
-              avatar: item.avatar,
-              express_com: item.express_com,
-              express_type: item.express_type,
-              college: item.college,
-              area: item.area,
-              detail: item.detail,
-              arrive_time: item.arrive_time,
-              total_price: item.total_price
-            }))
-          }
-        })
-
-        if (this.currentPage === this.totalPages) {
-          this.allLoaded = true
-        }
-
-        this.$store.commit('UPDATE_LOADING_STATUS', { status: false })
-      }, 500)
+      this.loading = false
     },
     // 查看任务详情, 非配送员无法进入
     toDetail (id) {
@@ -162,9 +157,6 @@ export default {
       this.currentPage = 0
       this.allLoaded = false
       this.$router.push({ path: '/staff/mission/list/' + this.status[index].value })
-    },
-    async handleBottomChange (status) {
-      this.bottomStatus = status
     }
   }
 }
@@ -177,6 +169,8 @@ export default {
   border-radius: 50%;
 }
 .loadmore-bottom{
+  margin: 8px auto;
   text-align: center;
+  background-color: #fff;
 }
 </style>
